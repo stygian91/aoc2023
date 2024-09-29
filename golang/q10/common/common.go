3804ccd69eb59ae14d69aecfe14d281ec83ed89a
Styg <kioravuser@gmail.com>
2024-09-29 16:05:27 +0300

    WIP

--- golang/q10/common/common.go
+++ golang/q10/common/common.go
@@ -1,15 +1,27 @@
 package common
 
-import "aoc2023/utils/math"
+import (
+	m "aoc2023/utils/math"
+	"slices"
+)
 
 type Tile = rune
 
 type Board struct {
 	Tiles  [][]Tile
-	Start  math.Vec2
+	Start  m.Vec2
 	Width  int
 	Height int
 }
+
+type Direction int
+
+const (
+	North Direction = iota
+	East
+	South
+	West
+)
 
 const (
 	Vertical    Tile = '|'
@@ -57,7 +69,7 @@ func Parse(lines []string) Board {
 		for x, ch := range line {
 			tile := TileFromRune(ch)
 			if tile == Start {
-				board.Start = math.Vec2{X: x, Y: y}
+				board.Start = m.Vec2{X: x, Y: y}
 			}
 			row = append(row, tile)
 		}
@@ -67,4 +79,43 @@ func Parse(lines []string) Board {
 	board.Tiles = tiles
 
 	return board
+}
+
+var validNorth = []Tile{Vertical, SouthToEast, SouthToWest}
+var validSouth = []Tile{Vertical, NorthToEast, NorthToWest}
+var validEast = []Tile{Horizontal, NorthToWest, SouthToWest}
+var validWest = []Tile{Horizontal, NorthToEast, SouthToEast}
+
+func (this Board) IsInBound(pos m.Vec2) bool {
+	return m.IsInBound(pos.X, 0, this.Width-1) && m.IsInBound(pos.Y, 0, this.Height-1)
+}
+
+func (this Board) IsValid(validTiles []Tile, pos m.Vec2) bool {
+	return this.IsInBound(pos) && slices.Contains(validTiles, this.Tiles[pos.Y][pos.X])
+}
+
+func (this Board) StartingPipes() []m.Vec2 {
+	res := []m.Vec2{}
+
+	northPos := m.Vec2{X: this.Start.X, Y: this.Start.Y - 1}
+	if this.IsValid(validNorth, northPos) {
+		res = append(res, northPos)
+	}
+
+	eastPos := m.Vec2{X: this.Start.X + 1, Y: this.Start.Y}
+	if this.IsValid(validEast, eastPos) {
+		res = append(res, eastPos)
+	}
+
+	southPos := m.Vec2{X: this.Start.X, Y: this.Start.Y + 1}
+	if this.IsValid(validSouth, southPos) {
+		res = append(res, southPos)
+	}
+
+	westPos := m.Vec2{X: this.Start.X - 1, Y: this.Start.Y}
+	if this.IsValid(validWest, westPos) {
+		res = append(res, westPos)
+	}
+
+	return res
 }
--- golang/q10/part1/part1.go
+++ golang/q10/part1/part1.go
@@ -1,10 +1,21 @@
 package part1
 
 import (
-	"aoc2023/q10/common"
+	c "aoc2023/q10/common"
+	"aoc2023/utils/math"
 	"fmt"
 )
 
-func Part1(board common.Board) {
-	fmt.Printf("%+v\n", board)
+type state struct {
+	visited []math.Vec2
 }
+
+func Part1(board c.Board) {
+	starting := board.StartingPipes()
+	if len(starting) != 2 {
+		panic("Expected 2 starting pipes")
+	}
+
+	fmt.Printf("starting: %#+v", starting)
+}
+
